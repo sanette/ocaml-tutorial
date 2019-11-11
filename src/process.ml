4f94ef254688abebf14d1de91c664b203851ea00
sanette <sanette-linux@laposte.net>
2019-11-11 12:16:46 +0100

    prepare ocaml.org integration

--- src/process.ml
+++ src/process.ml
@@ -13,17 +13,26 @@ open Printf
 let debug = true
 let pr = if debug then print_endline else fun _ -> ()
                                                    
-(* Set this to the directory where to find the html sources of all versions *)
+(* Set this to the directory where to find the html sources of all versions: *)
 let html_maindir = "html"
-
-(* Where to save th modified html files *)
-let docs_dir version = Filename.concat "docs" version
-let docs_file version = Filename.concat (docs_dir version)
-
+(* Set this to the destination directory: *)
+let docs_maindir = "docs"
+let ocamlorg_maindir = "ocamlorg"
+(* Header for ocaml.org md files. *)
+let md_head = "<!-- ((! set title Manual !)) ((! set documentation !)) ((! set manual !)) ((! set nobreadcrumb !)) -->\n"
+  
 (* Where to get the original html files *)
 let html_dir version = Filename.concat version "htmlman"
                        |> Filename.concat html_maindir
 let html_file version = Filename.concat (html_dir version)
+
+(* Where to save the modified html files *)
+let docs_dir version = Filename.concat docs_maindir version
+let docs_file version = Filename.concat (docs_dir version)
+
+(* Where to save the ocamlorg files *)
+let ocamlorg_dir version = Filename.concat ocamlorg_maindir version
+let ocamlorg_file version = Filename.concat (ocamlorg_dir version)
 
 (* Return next html element. *)
 let rec next node =
@@ -37,7 +46,7 @@ let rec next node =
 let remove_number s =
   Str.global_replace (Str.regexp ".+  ") "" s
 
-(* Scan the index and return the list of chapters (file, title) *)
+(* Scan the index and return the list of chapters (title, file) *)
 let index version =
   let html = open_in (html_file version "index.html")
              |> read_channel in
@@ -52,7 +61,7 @@ let index version =
     assert (name ul = "ul");
     ul $$ "a"
     |> fold (fun list a ->
-        (R.attribute "href" a, R.leaf_text a |> remove_number) :: list) []
+        (R.leaf_text a |> remove_number, R.attribute "href" a) :: list) []
     |> List.rev in
   tutorials   
 
@@ -140,7 +149,7 @@ let convert version chapters (title, file) =
   wrap body (create_element "body");
   let body = soup $ "body" in
   let dummy = body $ "body" in
-  wrap dummy (create_element "div" ~class_:"content");
+  wrap dummy (create_element "div" ~classes:["manual"; "content"]);
   let body = body $ "body" in
   unwrap body;
   (* let dummy = create_element "div" ~attributes:["id", "top"] in
@@ -165,6 +174,11 @@ let convert version chapters (title, file) =
   let li = create_element "li" ~class_:"top" in
   append_child li a;
   prepend_child toc li;
+  let toc_title = create_element "div" ~class_:"toc_title" in
+  let a = create_element "a" ~inner_text:("Version " ^ version)
+      ~attributes:["href", "../index.html"] in
+  append_child toc_title a;
+  prepend_child toc toc_title;
 
   (* Create new menu *)
   let menu = create_element "ul" ~class_:"tutos_menu" in
@@ -229,30 +243,66 @@ let convert version chapters (title, file) =
 
   (* Save new html file *)
   let new_html= to_string soup in
-  write_file (docs_file version file) new_html
+  write_file (docs_file version file) new_html;
+  (* Save ocamlorg md file *)
+  let md = Filename.basename file
+           |> Filename.remove_extension in
+  let md = md ^ ".md" in
+  soup $ "div.manual" |> to_string
+  |> (^) md_head 
+  |> write_file (ocamlorg_file version md)
+
+(* Create "index.html" (for standalone version) 
+      and "index.md"   (for ocaml.org) *)
+let make_index versions =
+  let html = read_channel @@ open_in @@ Filename.concat "src" "index.html" in
+  let soup = parse html in
+  let ul = soup $ "ul.versions" in
+  List.iter (fun (version, file) ->
+      let v = "Version " ^ version in
+      let file = Filename.concat version file in
+      let li = create_element "li" in
+      let a = create_element ~attributes:["href", file] ~inner_text:v "a" in
+      append_child li a;
+      append_child ul li) versions;
+  write_file (Filename.concat docs_maindir "index.html") (to_string soup);
+  soup $ "div.manual" |> to_string |> (^) md_head 
+  |> write_file (Filename.concat ocamlorg_maindir "index.md")
+  
+let sys_cp file dst =
+  if Sys.command (sprintf "cp %s %s" file dst) <> 0
+  then failwith ("Could not copy " ^ file)
+
+let sys_mkdir dir =
+  if Sys.command (sprintf "mkdir -p %s" dir) <> 0
+  then failwith ("Could not create directory" ^ dir)
 
 (* Download version of the manual if the dir does not exist yet. *)
+(* Remark: remove the html_maindir to force downloading everything. *)
 let download_version version =
   let pwd = Sys.getcwd () in
   begin try
-  let dir = Filename.concat html_maindir version in
-  if not (Sys.file_exists dir)
-  then if Sys.command (sprintf "mkdir %s" dir) <> 0
-    then failwith ("Could not create " ^ dir)
-    else let url = sprintf
-             "http://caml.inria.fr/distrib/ocaml-%s/ocaml-%s-refman-html.tar.gz"
-             version version in
-      let tmp = Filename.temp_file version ".tar.gz" in
-      let name = Filename.basename tmp in
-      if Sys.command (sprintf "wget %s -O %s" url tmp) <> 0
-      then failwith ("Could not download manual at " ^ url)
-      else if Sys.command (sprintf "mv %s %s/" tmp dir) <> 0
-      then failwith (sprintf "Could not move manual %s to %s." tmp dir)
-      else let () = Sys.chdir dir in
-        if Sys.command (sprintf "tar xvf %s" name) <> 0
-        then failwith (sprintf "Could not extract %s." name)
-        else if Sys.command (sprintf "rm %s" name) <> 0
-        then failwith ("Could not remove " ^ name)
+      let dir = Filename.concat html_maindir version in
+      if not (Sys.file_exists dir)
+      then begin
+        sys_mkdir dir;
+        let url = sprintf
+            "http://caml.inria.fr/distrib/ocaml-%s/ocaml-%s-refman-html.tar.gz"
+            version version in
+        let tmp = Filename.temp_file version ".tar.gz" in
+        let name = Filename.basename tmp in
+        if Sys.command (sprintf "wget %s -O %s" url tmp) <> 0
+        then failwith ("Could not download manual at " ^ url)
+        else begin
+          (* Sys.rename won't work between different partitions... *)
+          sys_cp tmp (Filename.concat dir name);
+          Sys.remove tmp;
+          Sys.chdir dir;
+          if Sys.command (sprintf "tar xvf %s" name) <> 0
+          then failwith (sprintf "Could not extract %s." name)
+          else Sys.remove name
+        end
+      end
     with
     | e -> Sys.chdir pwd; raise e
   end;
@@ -260,41 +310,54 @@ let download_version version =
 
 let download_versions = List.iter download_version
 
-(* Completely process the given version of the manual *)
+(* Completely process the given version of the manual.
+   Returns the name of the main html file. *)
 let process version =
   print_endline (sprintf "\nProcessing version %s...\n" version);
 
   pr (sprintf "Current directory is: %s" (Sys.getcwd ()));
-  if Sys.command (sprintf "mkdir -p %s" (docs_dir version)) <> 0
-  then failwith ("Could not create directory" ^ (docs_dir version));
+  sys_mkdir (docs_dir version);
+  sys_mkdir (ocamlorg_dir version);
 
   pr "* Copying files";
   let to_copy = ["colour-logo-gray.svg"; "manual.css"] in
   List.iter (fun file ->
       pr file;
-      if Sys.command (sprintf "cp src/%s %s" file (docs_file version file)) <> 0
-      then failwith ("Could not copy " ^ file)) to_copy;
+      sys_cp (Filename.concat "src/" file) (docs_file version file)
+    ) to_copy;
 
   pr "* Scanning index";
-  let chapters = index version |> List.map (fun (f,n) -> (n,f)) in
+  let chapters = index version in
 
   pr "* Processing chapters";
   List.iter (convert version chapters) chapters;
 
-  pr "* Create main link";
-  let link = "part1.html" in
-  if not (Sys.file_exists (docs_file version link))
-  then let _,entry_file = List.hd chapters in
-    if Sys.command
-        (sprintf "ln -s %s %s" entry_file (docs_file version link))
-       <> 0
-    then failwith (sprintf "Could not create symlink to %s/%s"
-                     (docs_dir version) entry_file)
+  (* pr "* Create main link";
+   * let link = "part1.html" in
+   * [docs_file version; ocamlorg_file version]
+   * |> List.iter (fun dir ->
+   *     if not (Sys.file_exists (dir link))
+   *     then let _,entry_file = List.hd chapters in
+   *       if Sys.command
+   *           (sprintf "ln -s %s %s" entry_file (dir link)) <> 0
+   *       then failwith (sprintf "Could not create symlink to %s" (dir link)))
+  *)
 
+  snd (List.hd chapters)
+  
+  
 (*********************************************************************)
         
 let () = 
   let all_versions = Array.init 10 (fun i -> sprintf "4.%02u" i)
                      |> Array.to_list in
   download_versions all_versions;
-  List.iter process all_versions
+  List.iter (fun file ->
+      sys_cp (Filename.concat "src" file) (Filename.concat docs_maindir file))
+    ["colour-logo-gray.svg"; "index.html"; "manual.css"];
+
+  let versions = List.map (fun v -> v, process v) all_versions in
+  pr "* Make index";
+  make_index (List.rev versions);
+  
+  pr "DONE."
